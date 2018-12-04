34e16fa5e51f0d8ef401bfcdcbb7af45f6246748
liqinglin <liql@bobol.com>
2018-12-04 18:04:36 +0800

    qiniu

---
+++ server/tasks/qiniu.js
@@ -0,0 +1,6 @@
+let movies = [{
+  video: 'http://vt1.doubanio.com/201812041704/ca1ade81790827fbb3a6629d9231e61e/view/movie/M/402390838.mp4',
+  doubanId: '27603700',
+  cover: 'https://img1.doubanio.com/img/trailer/medium/2540984268.jpg',
+  poster: 'https://img1.doubanio.com/view/photo/l_ratio_poster/public/p2540548908.jpg'
+}];
