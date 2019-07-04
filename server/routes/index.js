f661f67af28b40c3ac2dea0040145dc116c5496d
liqinglin <liql@bobol.com>
2019-07-04 18:01:57 +0800

    router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,19 @@
 const Router = require('koa-router')
-const Movie = mongoose.model('Movie') 
-
+const mongoose = require('mongoose')
 const router = new Router()
 
-router.get('/movies/all', async (ctx, next) => {
-  const movies = await Movie.find({}).sort({
-    'meta.createdAt': -1
-  })
+router
+  .get('/movies', async (ctx, next) => {
+    const Movie = mongoose.model('Movie') 
+    const movies = await Movie.find({}).sort({
+      'meta.createdAt': -1
+    })
 
-  ctx.body = {
-    movies
-  }
-})
+    ctx.body = {
+      movies
+    }
+  })
+  .post()
 
 router.get('/movies/detail/:id', async (ctx, next) => {
   const Movie = mongoose.model('Movie')
